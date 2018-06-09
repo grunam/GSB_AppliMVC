4ee25d04318fcee04d8d282ea30db54baf20c821
Namik Tiab <tiabnamik@gmail.com>
2018-06-09 15:16:37 +0200

    Version stable V 1.0.13
    -Maj des tests unitaires

---
+++ tests_fonctionnels/newSeleneseTest.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+require_once 'PHPUnit/Extensions/SeleniumTestCase.php';
+
+/**
+ * Description of newSeleneseTest
+ *
+ * @author tiabn
+ */
+class newSeleneseTest extends PHPUnit_Extensions_SeleniumTestCase {
+
+    function setUp() {
+        $this->setBrowser("*firefox");
+        $this->setBrowserUrl("http://localhost/");
+    }
+
+    function testMyTestCase() {
+        $this->open("/");
+    }
+
+}
+
+?>
